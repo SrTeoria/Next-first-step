2ac4fbc3b4eaa3eb7b20d57f13e1b411897275a4
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2022-04-13 14:38:08 -0500

    Se creo el primer layout, se aplico a todas las vistas y se modificaron los estilos globales

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,17 +1,17 @@
 import Link from 'next/link'
-import { MainLayout } from '../components/layouts/MainLayout'
+import { MainLayout } from '../../components/layouts/MainLayout'
 
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <MainLayout>
-      <h1>About Page</h1>
+      <h1>Contact Page</h1>
       <h1 className={'title'}>
         Ir a <Link href="/">Home</Link>
       </h1>
 
       <p className={'description'}>
         Get started by editing{' '}
-        <code className={'code'}>pages/about.jsx</code>
+        <code className={'code'}>pages/contact.jsx</code>
       </p>
     </MainLayout>
   )
